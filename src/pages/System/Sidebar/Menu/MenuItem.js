51dd3639ec6028132e26f743818590e1b15a146f
@anhnv99 <vietanhhappy99@gmail.com>
2022-08-04 12:08:10 +0700

    lesson 26: build menu item system manage

--- src/pages/System/Sidebar/Menu/MenuItem.js
+++ src/pages/System/Sidebar/Menu/MenuItem.js
@@ -1,17 +1,31 @@
 import classNames from 'classnames/bind';
 import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 
 import styles from './MenuItem.module.scss';
 
 const cx = classNames.bind(styles);
-function MenuItem({ title, to, icon }) {
+function MenuItem({ title, icon }) {
     return (
         <ul>
-            <li>
-                <NavLink className={(nav) => cx('menu-item', { active: nav.isActive })} to={to}>
+            <li className={cx('menu-item')}>
+                {/* <div></div> */}
+                <div className={cx('title-icon')}>
                     <span className={cx('icon')}>{icon}</span>
                     <span className={cx('title')}>{title}</span>
-                </NavLink>
+                </div>
+                <ul className={cx('list-item')}>
+                    <li>
+                        <NavLink className={cx('link-item')} to={'/system/user-manage'}>
+                            CRUD User
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={cx('link-item')} to={'/system/user-manage'}>
+                            Redux CRUD
+                        </NavLink>
+                    </li>
+                </ul>
             </li>
         </ul>
     );
--- src/pages/System/Sidebar/Sidebar.js
+++ src/pages/System/Sidebar/Sidebar.js
@@ -1,6 +1,5 @@
 import classNames from 'classnames/bind';
 import styles from './Sidebar.module.scss';
-import config from '~/config';
 import MenuItem from './Menu/MenuItem';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook, faHome, faLightbulb } from '@fortawesome/free-solid-svg-icons';
@@ -13,10 +12,10 @@ function Sidebar() {
                 <span>Happy24h</span>
             </div>
             <div className={cx('sidebar-menu')}>
-                <MenuItem title="Home" to={config.routes.home} icon={<FontAwesomeIcon icon={faHome} />} />
-                <MenuItem title="Library" to={config.routes.library} icon={<FontAwesomeIcon icon={faBook} />} />
-                <MenuItem title="Blog" to={config.routes.blog} icon={<FontAwesomeIcon icon={faBlogger} />} />
-                <MenuItem title="Learn" to={config.routes.learn} icon={<FontAwesomeIcon icon={faLightbulb} />} />
+                <MenuItem title="Home" icon={<FontAwesomeIcon icon={faHome} />} />
+                <MenuItem title="Library" icon={<FontAwesomeIcon icon={faBook} />} />
+                <MenuItem title="Blog" icon={<FontAwesomeIcon icon={faBlogger} />} />
+                <MenuItem title="Learn" icon={<FontAwesomeIcon icon={faLightbulb} />} />
             </div>
         </nav>
     );
